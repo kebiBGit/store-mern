17d3c157d1cd3806d4a6bb9dc89247a5ffc658bf
kebiBGit <173374658+kebiBGit@users.noreply.github.com>
2026-09-14 00:02:14 +0545

    Homepage and createpage

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,19 @@
-import { Container, Flex, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
+import { FaMoon, FaRegPlusSquare, FaSun } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <Container maxW={"1140px"} px={4}>
+    <Container maxW={"1920px"} px={4}>
       <Flex
         h={16}
         alignItems={"center"}
@@ -12,7 +23,28 @@ const Navbar = () => {
           sm: "row",
         }}
       >
-        <Text></Text>
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+        >
+          <Link to={"/"}>Product Store 🛒</Link>
+        </Text>
+
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/create"}>
+            <Button>
+              <FaRegPlusSquare fontSize={20} />
+            </Button>
+          </Link>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <FaMoon /> : <FaSun />}
+          </Button>
+        </HStack>
+
         <Text></Text>
       </Flex>
     </Container>
